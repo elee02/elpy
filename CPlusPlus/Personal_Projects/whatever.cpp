595671eb1fd72e656ab8dc5fb5e1964fdc8b06b7
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-09-21 00:25:03 +0900

    do it all

---
+++ CPlusPlus/Personal_Projects/whatever.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    for (auto& s : v) {
+        cout << s << endl;
+    }
+    return 0;
+}
