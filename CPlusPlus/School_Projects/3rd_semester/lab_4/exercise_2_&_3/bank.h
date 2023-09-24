7d57cbda60d61c42a133db417e7a759668ecdccd
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-09-25 02:08:11 +0900

    latest

--- CPlusPlus/School_Projects/3rd_semester/lab_4/exercise_2_&_3/bank.h
+++ CPlusPlus/School_Projects/3rd_semester/lab_4/exercise_2_&_3/bank.h
@@ -4,13 +4,14 @@
 void ShowMenu(void);
 void MakeAccount(void);
 void DepositMoney(void);
+void SendMoney(void); // Added
 void WithdrawMoney(void);
 void ShowAllAccInfo(void);
 void DeleteAccount(void);
 void SendMoney(void);
 int GetAccIdx(int);
 
-     // scoped enumeration cannot be used as a constant expression with switch statement
+enum class bank {MAKE = 1, DEPOSIT, SEND, WITHDRAW, INQUIRE, DELETE, EXIT}; // scoped enumeration cannot be used as a constant expression with switch statement
 const int NAME_LEN = 40;
 const int MAX_ACC_NUM = 100;
 
