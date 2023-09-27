382f67ce0d53ba9ac07665ee12251c6c0fea98a4
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-09-28 01:09:40 +0900

    ex02_final

--- CPlusPlus/School_Projects/3rd_semester/lab_5/exercise_2/main.cpp
+++ CPlusPlus/School_Projects/3rd_semester/lab_5/exercise_2/main.cpp
@@ -16,12 +16,23 @@ int main() {
     if (!pos2.InitMembers(5, 9))
         cout << "pos2: Failure of Init" << endl;
 
+    cout << "Enter two points:" << endl;
+    int pos[4];
+    cin >> pos[0] >> pos[1] >> pos[2] >> pos[3];
+    pos1.SetX(pos[0]);
+    pos1.SetY(pos[1]);
+    pos2.SetX(pos[2]);
+    pos2.SetY(pos[3]);
+
     Rectangle rec;
     if (!rec.InitMembers(pos2, pos1))
         cout << "rec-1: Failure of Init" << endl;
+    
+    rec.ShowRecInfo();
+
     if (!rec.InitMembers(pos1, pos2))
         cout << "rec-2: Failure of Init" << endl;
-
+        
     rec.ShowRecInfo();
 
     return 0;
