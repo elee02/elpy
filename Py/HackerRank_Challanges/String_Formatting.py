98437fd7ee6e3805a9f49b0833dba001283214ab
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-10-01 01:19:23 +0900

    hackerrank_string_formatting

--- Py/HackerRank_Challanges/String_Formatting.py
+++ Py/HackerRank_Challanges/String_Formatting.py
@@ -1,11 +1,12 @@
 def print_formatted(number):
     # your code goes here
     f_txt = ""
-    for i in range(number + 1):
-        f_dec = str(i).rjust(len(bin(i)) + 1)
-        f_oct = oct(i)[2:].rjust(6)
-        f_hex = hex(i)[2:].upper().rjust(6)
-        f_bin = bin(i)[2:].rjust(6)
+    for i in range(1, number + 1):
+        width = len(bin(number)[2:])
+        f_dec = str(i).rjust(width) + " "
+        f_oct = oct(i)[2:].rjust(width) + " "
+        f_hex = hex(i)[2:].upper().rjust(width) + " "
+        f_bin = bin(i)[2:].rjust(width)
         f_txt += f_dec + f_oct + f_hex + f_bin + "\n"
     print(f_txt)
 
