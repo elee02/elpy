b6b2805f171bb68044fb45c40926ca2cef4c9c4c
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-10-02 16:57:10 +0900

    The Minion Game

--- Py/HackerRank_Challanges/The_Minion_Game.py
+++ Py/HackerRank_Challanges/The_Minion_Game.py
@@ -1,6 +1,21 @@
 def minion_game(string):
     # your code goes here
-    
-if __name__ == '__main__':
+    consonant = "bcdfghjklmnpqrstvwxyz".upper()
+    vowel = "aeiou".upper()
+    numStuart = 0
+    numKevin = 0
+    for idx, let in enumerate(string):
+        if let in consonant:
+            numStuart += len(string) - idx
+        if let in vowel:
+            numKevin += len(string) - idx
+    if numStuart > numKevin:
+        print("Stuart ", numStuart)
+    elif numKevin > numStuart:
+        print("Kevin ", numKevin)
+    else:
+        print("Draw")
+
+if __name__ == "__main__":
     s = input()
     minion_game(s)
