ff71c5c15812b0c71e6453ae18751375620891d6
RASULOV ELYOR DUSYOR UGLI <112889202+elee02@users.noreply.github.com>
2023-10-15 07:12:55 +0000

    lab_7_ex_3

---
+++ CPlusPlus/School_Projects/3rd_semester/lab_7/exercise_3/bank.cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+#include "bank.h"
+using namespace std;
+
+int Account::getID() const {
+	return accID;
+}
+void Account::setID(int new_id) {
+	accID = new_id;
+}
+int Account::getBalance() const {
+	return balance;
+}
+void Account::setBalance(int new_balance) {
+	balance = new_balance;
+}
+char* Account::getName() const {
+	return cusName;
+}
+void Account::setName(const char* new_name) {
+	for (int i = 0; i < NAME_LEN; i++) {
+		if (new_name[i] == '\0') break;
+		cusName[i] = new_name[i];
+	}
+}
+
+Account accArr[MAX_ACC_NUM]; // Account array
+int accNum = 0;      // # of accounts
+
+void ShowMenu(void) {
+	cout << "-----Menu------" << endl;
+	cout << "1. Make Accout" << endl;
+	cout << "2. Deposit" << endl;
+	cout << "3. Send Money" << endl; // Added
+	cout << "4. Withdrawal" << endl;
+	cout << "5. Display all" << endl;
+	cout << "6. Delete account" << endl;
+	cout << "7. Exit program" << endl;
+}
+
+void MakeAccount(void) {
+	int id;
+	char name[NAME_LEN];
+	int balance;
+
+	if (accNum >= MAX_ACC_NUM) {
+		cout << "Sorry! cannot make an accout anymore." << endl;
+		return;
+	}
+
+	cout << "[Make Account]" << endl;
+	cout << "Account ID: ";
+	cin >> id;
+	cin.ignore(100, '\n');
+	cout << "Customer Name: ";
+	cin.getline(name,40);
+	cout << "Deposit amount: ";
+	cin >> balance;
+	cout << endl;
+
+	if (GetAccIdx(id) != -1) {
+		cout << "Error: Existing account ID" << endl << endl;
+		return;
+	}
+
+	accArr[accNum].setID(id);
+	accArr[accNum].setBalance(balance);
+	accArr[accNum].setName(name);
+	accNum++;
+}
+
+void DepositMoney(void) {
+	int money;
+	int id;
+	cout << "[Deposit]" << endl;
+	cout << "Account ID: ";
+	cin >> id;
+	cout << "Deposit amount: ";
+	cin >> money;
+
+	for (int i = 0; i < accNum; i++) {
+		if (accArr[i].getID() == id) {
+			accArr[i].setBalance(accArr[i].getBalance() + money);
+			cout << "Deposit completed" << endl << endl;
+			return;
+		}
+	}
+	cout << "This ID is not valid." << endl << endl;
+}
+
+void WithdrawMoney(void) {
+	int money;
+	int id;
+	cout << "[Withdrawal]" << endl;
+	cout << "Account ID: ";
+	cin >> id;
+	cout << "Withdrawal amount: ";
+	cin >> money;
+
+	for (int i = 0; i < accNum; i++) {
+		if (accArr[i].getID() == id) {
+			if (accArr[i].getBalance() < money) {
+				cout << "Not enough balance" << endl << endl;
+				return;
+			}
+
+			accArr[i].setBalance(accArr[i].getBalance() - money);
+			cout << "Withdrawal completed" << endl << endl;
+			return;
+		}
+	}
+	cout << "This ID is not valid." << endl << endl;
+}
+
+void DeleteAccount(void) {
+	int id;
+	cout << "[Delete Account]" << endl;
+	cout << "Account ID (0 to delete all accounts): ";
+	cin >> id;
+
+	if (id == 0) {
+		accNum = 0;
+		cout << "All accounts have been deleted." << endl << endl;
+		return;
+	}
+
+	for (int i = 0; i < accNum; i++) {
+		if (accArr[i].getID() == id) {
+			for (int j = i; j < accNum - 1; j++) {
+				accArr[j] = accArr[j + 1];
+			}
+			accNum--;
+			cout << "Account " << id << " has been deleted." << endl << endl;
+			return;
+		}
+	}
+	cout << "Account " << id << " not found." << endl << endl;
+}
+
+void SendMoney(void) {
+	int money;
+	int sender, receiver;
+	cout << "[Send Money]" << endl;
+	cout << "Withdrawal Account ID: ";
+	cin >> sender;
+	cout << "Receiving Account: ";
+	cin >> receiver;
+	if (sender == receiver) {
+		cout << "Cannot send money to the same account." << endl << endl;
+		return;
+	}
+	cout << "Amount: ";
+	cin >> money;
+
+	for (int i = 0; i < accNum; i++) {
+		if (accArr[i].getID() == sender) {
+			if (accArr[i].getBalance() < money) {
+				cout << "Not enough balance" << endl << endl;
+				return;
+			}
+			for (int j = 0; j < accNum; j++) {
+				if (accArr[j].getID() == receiver) {
+					accArr[j].setBalance(accArr[j].getBalance() + money);
+					break;
+				}
+				if ((j == accNum - 1) && (accArr[j].getID() != receiver)) {
+					cout << "Invalid Receiving Account!" << endl << endl;
+					return;
+				}
+			}
+			accArr[i].setBalance(accArr[i].getBalance() - money);
+			cout << "Send completed" << endl << endl;
+			return;
+		}
+	}
+	cout << "Invalid Withdrawal Account!" << endl << endl;
+}
+
+void ShowAllAccInfo(void) {
+	for (int i = 0; i < accNum; i++) {
+		cout << "Account ID: " << accArr[i].getID() << endl;
+		cout << "Name: ";
+		char* namePtr = accArr[i].getName();
+		for (int j = 0; j < NAME_LEN; j++) {
+			if(namePtr[j] != '\0')
+				cout << namePtr[j];
+		}
+		cout << endl;
+		cout << "Balance: " << accArr[i].getBalance() << endl << endl;
+	}
+	if (accNum == 0) {
+		cout << "No accounts exist." << endl << endl;
+	}
+}
+
+int GetAccIdx(int id) {
+	for (int i = 0; i < accNum; i++) {
+		if (accArr[i].getID() == id) {
+			return i;
+		}
+	}
+	return -1;
+}
---
+++ CPlusPlus/School_Projects/3rd_semester/lab_7/exercise_3/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "bank.h"
+using namespace std;
+
+int main(void) {
+	int choice;
+
+	while (1) {
+		ShowMenu();
+		cout << "Select menu: ";
+		cin >> choice;
+		cout << endl;
+
+		switch (choice) {
+		case MAKE:
+			MakeAccount();
+			break;
+		case DEPOSIT:
+			DepositMoney();
+			break;
+		case SEND:
+			SendMoney();
+			break;
+		case WITHDRAW:
+			WithdrawMoney();
+			break;
+		case INQUIRE:
+			ShowAllAccInfo();
+			break;
+		case DELETE:
+			DeleteAccount();
+			break;
+		case EXIT:
+			return 0;
+		default:
+			cout << "Illegal selection.." << endl;
+		}
+	}
+	return 0;
+}
