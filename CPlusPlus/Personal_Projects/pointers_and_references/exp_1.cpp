8a1b36a8f967520b4fcf904900931d3ed7d14f44
RASULOV ELYOR DUSYOR UGLI <112889202+elee02@users.noreply.github.com>
2023-11-13 03:11:32 +0000

    references and pointers from prof's notes

---
+++ CPlusPlus/Personal_Projects/pointers_and_references/exp_1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+protected:
+    int m_value;
+
+public:
+    Base(int value) : m_value{value} {}
+
+    const string getName() const {
+        return "Base";
+    }
+
+    int getValue() const {
+        return m_value;
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived(int value) : Base{value} {}
+
+    const string getName() const {
+        return "Derived";
+    }
+
+    int getValueDoubled() const {
+        return m_value * 2;
+    }
+};
+
+int main() {
+    Derived derived{5};
+    cout << "derived is a " << derived.getName() << " and has value " << derived.getValue() << '\n';
+
+    Derived &rDerived = derived;
+    cout << "rDerived is a " << rDerived.getName() << " and has value " << rDerived.getValue() << '\n';
+
+    Derived *pDerived = &derived;
+    cout << "pDerived is a " << pDerived->getName() << " and has value " << pDerived->getValue() << '\n';
+
+    return 0;
+}
