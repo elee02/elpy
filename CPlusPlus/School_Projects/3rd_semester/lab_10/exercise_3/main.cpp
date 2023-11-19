6b3f774b7d2efd5875dc5e4850114195ec91de9d
RASULOV ELYOR DUSYOR UGLI <112889202+elee02@users.noreply.github.com>
2023-11-19 14:45:10 +0000

    Add bank program v0.5

---
+++ CPlusPlus/School_Projects/3rd_semester/lab_10/exercise_3/main.cpp
@@ -0,0 +1,44 @@
+/*
+Bank Program v0.4
+*/
+#include <iostream>
+#include "bank.h"
+using namespace std;
+
+int main(void) {
+	AccountHandler mn;
+	int choice;
+
+	while (true) {
+		mn.ShowMenu();
+		cout << "Select menu: ";
+		cin >> choice;
+		cout << endl;
+
+		switch (choice) {
+		case MAKE:
+			mn.MakeAccount();
+			break;
+		case DEPOSIT:
+			mn.DepositMoney();
+			break;
+		case SEND:
+			mn.SendMoney();
+			break;
+		case WITHDRAW:
+			mn.WithdrawMoney();
+			break;
+		case INQUIRE:
+			mn.ShowAllAccInfo();
+			break;
+		case DELETE:
+			mn.DeleteAccount();
+			break;
+		case EXIT:
+			return 0;
+		default:
+			cout << "Illegal selection.." << endl;
+		}
+	}
+	return 0;
+}
