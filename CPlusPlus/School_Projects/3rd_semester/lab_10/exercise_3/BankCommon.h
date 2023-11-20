db720255258dc7a9bd8d41c6b42e99a46e667774
RASULOV ELYOR DUSYOR UGLI <112889202+elee02@users.noreply.github.com>
2023-11-20 10:22:01 +0000

    Fix enum class declaration in BankCommon.h

--- CPlusPlus/School_Projects/3rd_semester/lab_10/exercise_3/BankCommon.h
+++ CPlusPlus/School_Projects/3rd_semester/lab_10/exercise_3/BankCommon.h
@@ -4,7 +4,7 @@ Bank Program v0.5
 #ifndef _BANKCOMMON_H_
 #define _BANKCOMMON_H_
 
-enum bank {MAKE = 1, DEPOSIT, SEND, WITHDRAW, INQUIRE, DELETE, EXIT}; // scoped enumeration cannot be used as a constant expression with switch statement
+enum class bank {MAKE = 1, DEPOSIT, SEND, WITHDRAW, INQUIRE, DELETE, EXIT}; // scoped enumeration cannot be used as a constant expression with switch statement
 const int NAME_LEN = 40;
 const int MAX_ACC_NUM = 100;
 
