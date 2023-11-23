6dcdf44d9b5e9d66c54d95c7adef9bd288caa8cd
RASULOV ELYOR DUSYOR UGLI <112889202+elee02@users.noreply.github.com>
2023-11-23 05:18:00 +0000

    Fix typo in Fraction class constructor parameter name

--- CPlusPlus/School_Projects/3rd_semester/lab_11/exercise_1/main.cpp
+++ CPlusPlus/School_Projects/3rd_semester/lab_11/exercise_1/main.cpp
@@ -7,17 +7,17 @@ private:
     int m_numerator;
     int m_denominator;
 public:
-    Fraction(int num = 0, int denum = 1) : m_numerator{num}, m_denominator{denum} {}
+    Fraction(int num = 0, int denom = 1) : m_numerator{num}, m_denominator{denom} {}
     int getNum() const {
         return m_numerator;
     }
-    int getDenum() const {
+    int getDenom() const {
         return m_denominator;
     }
 };
 
 ostream& operator<<(ostream& out, const Fraction& frac) {
-    out << frac.getNum() << "/" << frac.getDenum();
+    out << frac.getNum() << "/" << frac.getDenom();
     return out;
 }
 
