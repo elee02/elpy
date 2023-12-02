b6c97f028f19ed12f570e3fe9f5e9d37291997e6
elee02 <elyorrasulov1822@gmail.com>
2023-12-02 23:24:17 +0900

    Update main.cpp with string operations

--- CPlusPlus/School_Projects/3rd_semester/lab_12/exercise_1/main.cpp
+++ CPlusPlus/School_Projects/3rd_semester/lab_12/exercise_1/main.cpp
@@ -1,8 +1,22 @@
-￼
-2
-￼
-3
-￼
-4
-￼
-￼
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1 = "I like ";
+    string str2 = "string class";
+    string str3 = str1 + str2;
+
+    str1 += str2;
+    if (str1 == str3) {
+        cout << "The strings are the same" << endl;
+    } else {
+        cout << "The strings are different" << endl;
+    }
+
+    string str4;
+    cout << "Enter a text: ";
+    cin >> str4;
+    cout << "You entered: " << str4 << endl;
+    return 0;
+}
