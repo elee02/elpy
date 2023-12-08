9fde85c40bfa4fe72cbe1c6892b72748b4a1f9af
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-12-08 22:15:30 +0900

    changed the extension from .cpp to .ino

--- CPlusPlus/Personal_Projects/lcd_16x2_display_source/main.ino
+++ CPlusPlus/Personal_Projects/lcd_16x2_display_source/main.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal.h>
 #include <string.h>
-const int rs{12}, e{11}, d4{5}, d5{4}, d6{3}, d7{2};
 
+const int rs{12}, e{11}, d4{5}, d5{4}, d6{3}, d7{2};
 LiquidCrystal lcd{rs, e, d4, d5, d6, d7};
 const int numCols{16}, numRows{2};
 
