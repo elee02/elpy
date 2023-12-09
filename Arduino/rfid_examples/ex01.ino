836d8a87fe53a99dfb3103d6a4d5d9a6bd2cfb0b
Elyor Dusyor Ugli Rasulov <grayethan740@gmail.com>
2023-12-09 13:43:55 +0900

    Fix duplicate loop in ex01.ino

--- Arduino/rfid_examples/ex01.ino
+++ Arduino/rfid_examples/ex01.ino
@@ -33,3 +33,43 @@ void loop() {
         delay(1000); // Wait for 1 second before scanning for another card
     }
 }
+
+void loop() {
+    // Check if a new card is present
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+        // Get the UID of the card
+        String uid = "";
+        for (byte i = 0; i < rfid.uid.size; i++) {
+            uid += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+            uid += String(rfid.uid.uidByte[i], HEX);
+        }
+
+        Serial.print("Card UID: ");
+        Serial.println(uid);
+
+        rfid.PICC_HaltA(); // Halt PICC
+        rfid.PCD_StopCrypto1(); // Stop encryption on PCD
+
+        delay(1000); // Wait for 1 second before scanning for another card
+    }
+}
+
+void loop() {
+    // Check if a new card is present
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+        // Get the UID of the card
+        String uid = "";
+        for (byte i = 0; i < rfid.uid.size; i++) {
+            uid += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+            uid += String(rfid.uid.uidByte[i], HEX);
+        }
+
+        Serial.print("Card UID: ");
+        Serial.println(uid);
+
+        rfid.PICC_HaltA(); // Halt PICC
+        rfid.PCD_StopCrypto1(); // Stop encryption on PCD
+
+        delay(1000); // Wait for 1 second before scanning for another card
+    }
+}
