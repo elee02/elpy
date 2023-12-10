2af5940264044f9153f7d5adfaff637ff52e333e
elee02 <elyorrasulov1822@gmail.com>
2023-12-10 15:00:35 +0900

    Update system of equations with matrices

--- Py/Random_Projects/system_of_equations_with_matrices/system_of_equations_with_matrices.py
+++ Py/Random_Projects/system_of_equations_with_matrices/system_of_equations_with_matrices.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 # Define the coefficients matrix A
-A = np.array([[2, 1], [1, -3]])
+A = np.array([[1/4.7+2, -0.5, -1], 
+              [-0.5, 3, -1],
+              [1, 1, -2-1/4.7]])
 
 # Define the constants vector b
-b = np.array([5, -4])
+b = np.array([2.5, 5, 0])
 
 # Solve the system of equations
 x = np.linalg.solve(A, b)
