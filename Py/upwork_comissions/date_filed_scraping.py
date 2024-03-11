9f7f7b6d89221f01ff23f59fcb0160a5e8dbb2ee
elee02 <elyorrasulov1822@gmail.com>
2024-03-11 09:28:02 +0900

    Update URL in date_filed_scraping.py

--- Py/upwork_comissions/date_filed_scraping.py
+++ Py/upwork_comissions/date_filed_scraping.py
@@ -1,7 +1,10 @@
+"""
+This script is used to scrape the date filed from the upwork comissions link.
+"""
 import bs4
 import requests
 
-r = requests.get('https://beautiful-soup-4.readthedocs.io/en/latest/')
+r = requests.get('https://search.sunbiz.org/Inquiry/CorporationSearch/ByName')
 r.raise_for_status()
 
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
