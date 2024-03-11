7ceb3c676f4309340c209d56eecc59baa2ddaf13
elee02 <elyorrasulov1822@gmail.com>
2024-03-11 09:24:55 +0900

    Add web scraping functionality using BeautifulSoup

--- Py/upwork_comissions/date_filed_scraping.py
+++ Py/upwork_comissions/date_filed_scraping.py
@@ -1,1 +1,9 @@
-    
+import bs4
+import requests
+
+r = requests.get('https://beautiful-soup-4.readthedocs.io/en/latest/')
+r.raise_for_status()
+
+soup = bs4.BeautifulSoup(r.text, 'html.parser')
+
+print(soup.prettify())
