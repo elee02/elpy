d8e18e497bf5309ae081370e98d80cacc8ab89e7
elee02 <elyorrasulov1822@gmail.com>
2024-03-25 20:35:04 +0900

    Fix pattern matching algorithm and add necessary headers

--- CPlusPlus/School_Projects/4th_semester/data_structures_and_algorithms/class2/class6/second_pattern_matching_algo.cpp
+++ CPlusPlus/School_Projects/4th_semester/data_structures_and_algorithms/class2/class6/second_pattern_matching_algo.cpp
@@ -1,5 +1,8 @@
 // Second Pattern matching algorithm using graphs/tables
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstring>
 using namespace std;
 
 int F(int, int);
@@ -15,11 +18,11 @@ int states[6][3] = {
 };
 
 int main() {
-    char T[80] = "ffjjsjjdaaaeedcvvvcrreaavvbbabbaabaabbbaaabbaabbbfss";
+    char T[80] = "youcansayanything.comaabbaabbbfsseaavvbbabbaabaabbba";
     char P[80] = "aabbaa";
-    int K = 0, S = 0, I = 0, INDEX = -1;
+    int K = 0, S = 0, I = 0, INDEX = -1, NT = strlen(T), NP = strlen(P);
 
-    while ((S != -1) && (K <= sizeof(T) - 1)) {
+    while ((S != -1) && (K < NT)) {
         if (T[K] == 'a') {
             I = 0;
         } else if (T[K] == 'b') {
@@ -28,11 +31,11 @@ int main() {
             I = 2;
         }
         S = F(S, I);
-        K =+ 1; // exists with S_K+1
+        K += 1; // exits with S_K+1
     }
 
     if (S == -1) {
-        INDEX = (K - sizeof(P) + 1); // sizeof() => N+1
+        INDEX = (K - NP); // sizeof() => N+1
     // Plus, indexing starts with 0;
     } else {
         INDEX = -1;
@@ -41,7 +44,7 @@ int main() {
     if (INDEX != -1) {
         cout << "The index of the pattern '" << P << "' is " << INDEX << endl;
     } else {
-        cout << "The pattern does not exist.";
+        cout << "The pattern does not exist." << endl;
     }
 
     return 0;
