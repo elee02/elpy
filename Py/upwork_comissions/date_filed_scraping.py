330a94b87269da5b8baefee7864a9d5ea6aade33
elee02 <elyorrasulov1822@gmail.com>
2024-03-28 23:34:14 +0900

    Refactor date_filed_scraping.py script to include algorithm and update URL search

--- Py/upwork_comissions/date_filed_scraping.py
+++ Py/upwork_comissions/date_filed_scraping.py
@@ -1,10 +1,26 @@
 """
-This script is used to scrape the date filed from the upwork comissions link.
+Algorithm:
+
+1. Search the field replacing keywords in the URL
+
+2. Get all the names and URLs from the page and 
+the coming pages until the name does not match
+
+3. From each URL, get the date filed and add 
+the list of names and urls
+
+4. Sort the list by date filed
+
+5. Remove the entries more than 30 days old
+
+6. Print the list of names and URLs
 """
+
 import bs4
 import requests
 
-r = requests.get('https://search.sunbiz.org/Inquiry/CorporationSearch/ByName')
+name = "google"
+r = requests.get(f'https://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults/EntityName/{name}/Page1?searchNameOrder={name.upper()}')
 r.raise_for_status()
 
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
